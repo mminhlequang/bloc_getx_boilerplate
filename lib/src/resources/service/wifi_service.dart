8ed17877db20d4e64d48cc33dcb41272b05f1fc4
Minh Minh <mminh.lequang@gmail.com>
2022-06-21 12:09:49 +0700

    -

--- lib/src/resources/service/wifi_service.dart
+++ lib/src/resources/service/wifi_service.dart
@@ -7,11 +7,14 @@ typedef WifiListener = Function(bool enabled);
 class WifiService {
   StreamSubscription<ConnectivityResult>? _subscription;
 
+  static ConnectivityResult? _connectivityResult;
+
   WifiService({WifiListener? listener}) {
     if (_subscription != null) _subscription!.cancel();
     _subscription = Connectivity()
         .onConnectivityChanged
         .listen((ConnectivityResult result) {
+      _connectivityResult = result;
       // Got a new connectivity status!
       if (listener != null) {
         listener(result != ConnectivityResult.none);
@@ -20,12 +23,21 @@ class WifiService {
   }
 
   static Future<bool> isConnectivity() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
+    var connectivityResult =
+        _connectivityResult ?? await (Connectivity().checkConnectivity());
     return connectivityResult != ConnectivityResult.none;
   }
 
+  static Future<bool> isDisconnectWhenCallApi() async {
+    var connectivityResult =
+        _connectivityResult ?? await Connectivity().checkConnectivity();
+    bool status = connectivityResult == ConnectivityResult.none;
+    return status;
+  }
+
   static Future<bool> isDisconnect() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
+    var connectivityResult =
+        _connectivityResult ?? await Connectivity().checkConnectivity();
     return connectivityResult == ConnectivityResult.none;
   }
 
